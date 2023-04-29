99320e934637bdd019bce870cff1002ab6f098b3
yassinekhan <61456396+yassinekhan@users.noreply.github.com>
2023-04-29 15:55:44 +0200

    Add files via upload

---
+++ CloudnineApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.cloudnine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudnineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudnineApplication.class, args);
+    }
+
+}
